18a65475e6ae6eda36828e2213f50737ab8d9b66
Mauri Ruiz <mauri.ruiz@webbeds.com>
2026-01-27 12:35:45 +0100

    react formatter added & aerospace mve workspaces fix

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -5,10 +5,19 @@ return {
     opts.default_format_opts.lsp_format = "never"
 
     opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+    -- Backend
     opts.formatters_by_ft.rust = { "rustfmt" }
     opts.formatters_by_ft.go = { "gofmt" }
+
+    -- Frontend / React
+    opts.formatters_by_ft.javascript = { "prettier" }
+    opts.formatters_by_ft.javascriptreact = { "prettier" }
+    opts.formatters_by_ft.typescript = { "prettier" }
+    opts.formatters_by_ft.typescriptreact = { "prettier" }
+    opts.formatters_by_ft.html = { "prettier" }
+    opts.formatters_by_ft.css = { "prettier" }
 
     return opts
   end,
 }
-
